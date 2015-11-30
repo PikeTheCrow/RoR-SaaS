c5a1cea97776b02e70b04c13656863597e81175b
PikeTheCrow <pikethecrow@gmail.com>
2015-11-30 16:05:41 +0000

    Added mailer, controller and email template

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,25 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params) # creates a blacnk contacts object
+
+    if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+
+      ContactMailer.contact_email(name, email, body).deliver
+      
+      flash[:success] = "Glad you want to Game"
+      redirect_to new_contacts_path
+    else
+      flash[:danger] = "Fuck bro, something went wrong"
+      redirect_to new_contacts_path
+    end
   end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
